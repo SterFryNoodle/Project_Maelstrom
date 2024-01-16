ae6cfabaeb79f60efe8c12c234520d3295a59cc7
SterFryNoodle <revangcruz@yahoo.com>
2024-01-15 20:42:19 -0800

    transform in x-axis/changed camera position

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,6 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(0.1f, 0, 0);
     }
 }
