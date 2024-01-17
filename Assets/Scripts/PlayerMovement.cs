e3db843b6b40728c1336f1170909d036c7fdc17a
SterFryNoodle <revangcruz@yahoo.com>
2024-01-17 14:42:27 -0800

    changed moveSpeed from int to float

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] int moveSpeed = 5;
+    [SerializeField] float moveSpeed = 5;
 
     // Start is called before the first frame update
     void Start()
