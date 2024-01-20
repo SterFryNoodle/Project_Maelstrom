6d011b76a3363ab9034b766647a0aecc09fc0edd
SterFryNoodle <revangcruz@yahoo.com>
2024-01-19 20:33:06 -0800

    Created script to count # of collisions

--- Assets/Scripts/Scorer.cs
+++ Assets/Scripts/Scorer.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WallCollision : MonoBehaviour
+public class Scorer : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
-    {        
-        GetComponent<MeshRenderer>().material.color = Color.cyan;
+    {
+        
     }
 }
