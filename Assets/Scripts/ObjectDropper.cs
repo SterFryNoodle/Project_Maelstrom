75eb419d678e3783b563dd186c1ab28a2df87d53
SterFryNoodle <revangcruz@yahoo.com>
2024-01-19 22:40:54 -0800

    Renamed script to ObjectDropper; Added if-statement.

--- Assets/Scripts/ObjectDropper.cs
+++ Assets/Scripts/ObjectDropper.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TimeElapsed : MonoBehaviour
+public class ObjectDropper : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -14,5 +14,13 @@ public class TimeElapsed : MonoBehaviour
     void Update()
     {
         Debug.Log("Time passed: " + Time.time);
+        if (Time.time > 3)
+        {
+            // implement gravity to attached object.
+        }
+        else
+        {
+            // dont apply gravity.
+        }
     }
 }
