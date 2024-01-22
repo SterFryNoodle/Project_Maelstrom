914a0cafc18ed14d6ff26288c699ea5c92da1509
SterFryNoodle <revangcruz@yahoo.com>
2024-01-21 21:22:14 -0800

    Applied rotation motion to object

--- Assets/Scripts/SpinObject.cs
+++ Assets/Scripts/SpinObject.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SpinObject : MonoBehaviour
 {
+    [SerializeField] private float rotateX, rotateY = 1, rotateZ;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,6 @@ public class SpinObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(rotateX, rotateY, rotateZ);
     }
 }
